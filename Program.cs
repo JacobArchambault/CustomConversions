ebd93a9b36effe33ec11e1f2ff6ef0bf1ce19ff3
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-17 11:22:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace CustomConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Conversions *****\n");
+            // Make a Rectangle
+            Rectangle r = new Rectangle(15, 4);
+            Console.WriteLine(r.ToString());
+            r.Draw();
+
+            Console.WriteLine();
+
+            // Convert r into a Square based on the height of the Rectangle.
+            Square s = (Square)r;
+            Console.WriteLine(s.ToString());
+            s.Draw();
+
+            // Convert Rectangle to Square to invoke method.
+            Rectangle rect = new Rectangle(10, 5);
+            DrawSquare((Square)rect);
+
+            Console.ReadLine();
+
+
+        }
+
+        static void DrawSquare(Square sq)
+        {
+            Console.WriteLine(sq.ToString());
+            sq.Draw();
+        }
+
+    }
+    public struct Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle(int w, int h) : this()
+        {
+            Width = w;
+            Height = h;
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public override string ToString() => $"[Width = {Width}; Height = {Height}]";
+    }
+
+    public struct Square
+    {
+        public int Length { get; set; }
+        public Square(int l) : this()
+        {
+            Length = 1;
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public override string ToString() => $"[Length = {Length}]";
+
+        // Rectangles can be explicitly converted into Squares.
+        public static explicit operator Square(Rectangle r)
+        {
+            Square s = new Square { Length = r.Height };
+            return s;
+        }
+
+    }
+
+}
